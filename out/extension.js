5007f564701dbe06585bbd0b53330a0a701739f5
enoyao <enoyao@tencent.com>
2020-05-24 13:36:11 +0800

    update screenshot
    update screenshot

--- out/extension.js
+++ out/extension.js
@@ -1,11 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.deactivate = exports.activate = void 0;
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require("vscode");
-const rimraf = require('rimraf');
-const { exec } = require('child_process');
+const rimraf = require("rimraf");
+const { exec } = require("child_process");
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
@@ -15,32 +14,26 @@ function activate(context) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('delete-node-modules.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World from delete-node-modules!');
-    });
     let deleteNodeModules = vscode.commands.registerCommand("delete-node-modules.deleteNodeModules", (path) => {
         // Display a message box to the user
         rimraf(path.fsPath, (data) => {
             if (!data) {
                 // npm cache clean
-                vscode.window.showInformationMessage('Delete Node Modules Succeeded!');
-                exec('npm cache clean', (err, stdout, stderr) => {
+                vscode.window.showInformationMessage("Delete Node Modules Succeeded!");
+                exec("npm cache clean", (err, stdout, stderr) => {
                     if (err) {
-                        console.error('clean cache failed');
+                        console.error("clean cache failed");
                         return;
                     }
-                    console.error('clean cache succeeded');
+                    console.error("clean cache succeeded");
                 });
             }
             else {
-                vscode.window.showInformationMessage('Delete Node Modules Failed!');
+                vscode.window.showInformationMessage("Delete Node Modules Failed!");
             }
         });
     });
     context.subscriptions.push(deleteNodeModules);
-    context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 // this method is called when your extension is deactivated
--- src/extension.ts
+++ src/extension.ts
@@ -1,52 +1,47 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-const rimraf = require('rimraf');
-const { exec } = require('child_process');
+import * as vscode from "vscode";
+const rimraf = require("rimraf");
+const { exec } = require("child_process");
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "delete-node-modules" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "delete-node-modules" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let deleteNodeModules = vscode.commands.registerCommand(
+    "delete-node-modules.deleteNodeModules",
+    (path) => {
+      // Display a message box to the user
+      rimraf(path.fsPath, (data: any) => {
+        if (!data) {
+          // npm cache clean
+          vscode.window.showInformationMessage(
+            "Delete Node Modules Succeeded!"
+          );
+          exec("npm cache clean", (err: any, stdout: any, stderr: any) => {
+            if (err) {
+              console.error("clean cache failed");
+              return;
+            }
+            console.error("clean cache succeeded");
+          });
+        } else {
+          vscode.window.showInformationMessage("Delete Node Modules Failed!");
+        }
+      });
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('delete-node-modules.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from delete-node-modules!');
-	});
-
-	let deleteNodeModules = vscode.commands.registerCommand(
-		"delete-node-modules.deleteNodeModules",
-		(path) => {
-			// Display a message box to the user
-			rimraf(path.fsPath, (data: any) => {
-				if (!data) {
-					// npm cache clean
-					vscode.window.showInformationMessage('Delete Node Modules Succeeded!');
-					exec('npm cache clean', (err: any, stdout: any, stderr: any) => {
-						if (err) {
-							console.error('clean cache failed');
-							return;
-						}
-						console.error('clean cache succeeded');
-					});
-				} else {
-					vscode.window.showInformationMessage('Delete Node Modules Failed!');
-				}
-			})
-		}
-	);
-
-	context.subscriptions.push(deleteNodeModules);
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(deleteNodeModules);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
